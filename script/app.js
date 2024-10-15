444d9aa7d511b17ac34019fd68acea232811fde7
GabrielHidaN <victorbielgv7@gmail.com>
2024-10-15 12:52:43 -0300

    add box with text  on page

--- script/app.js
+++ script/app.js
@@ -1,4 +1,10 @@
 const pesquisarUser = ()=>{
     var valor = document.getElementById("username").value;
-    alert("O valor inserido é: " + valor);
+    if(valor.length == 0){
+        alert("você nao pode deixar o campo vazio")
+    }
+    else{
+        alert("O valor inserido é: " + valor);
+    }
+
 }
